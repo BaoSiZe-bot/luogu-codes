0d4ca1052182f8893676f78e11990cae1cad8350
Size Bao <baosize@hotmail.com>
2025-01-13 17:00:50 +0800

    Aced UVA1382.

--- UVA1328 Period.cpp
+++ UVA1328 Period.cpp
@@ -1,6 +1,5 @@
 #include <cstdio>
 const int N = 1e6 + 5;
-using ll = long long;
 char s[N];
 int nx[N];
 int main()
@@ -8,9 +7,7 @@ int main()
     int n, id = 0;
     while (scanf("%d", &n), n)
     {
-        if (id)
-            printf("\n\n");
-        printf("Test case #%d", ++id);
+        printf("Test case #%d\n", ++id);
         scanf("%s", s + 1);
         int p = 0;
         for (int i = 2; i <= n; ++i)
@@ -21,8 +18,9 @@ int main()
                 ++p;
             nx[i] = p;
             if (p && i % (i - p) == 0)
-                printf("\n%d %d", i, i / (i - p));
+                printf("%d %d\n", i, i / (i - p));
         }
+        printf("\n");
     }
     return 0;
 }
