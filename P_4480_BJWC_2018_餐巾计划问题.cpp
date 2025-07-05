aa2870f3ef1c57962916f0f96d33ed3913408eb8
Chang Han-jui <baosize@hotmail.com>
2025-07-05 17:45:15 +0800

    Aced P4480

---
+++ P_4480_BJWC_2018_餐巾计划问题.cpp
@@ -0,0 +1,139 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+
+using std::min;
+using ll = long long;
+
+const ll INF = 1e18;
+const int N = 6e6 + 5, M = 2e7 + 5;
+
+struct Edge
+{
+    ll cc;
+    int v, p;
+} e[M << 1];
+int cnt;
+int n, m, res = 0;
+int q[N], lev[N], cur[N], ed[N];
+inline void add(int u, int v, ll c)
+{
+    e[cnt] = {c, v, ed[u]};
+    ed[u] = cnt++;
+    e[cnt] = {0, u, ed[v]};
+    ed[v] = cnt++;
+}
+
+inline bool spfa(int s, int t)
+{
+    int hd = 1, tl = 0;
+    for (int i = s; i <= t; ++i)
+    {
+        lev[i] = -1;
+        cur[i] = ed[i];
+    }
+    lev[q[++tl] = s] = 0;
+
+    while (hd <= tl)
+    {
+        int nw = q[hd++];
+        for (int i = ed[nw]; ~i; i = e[i].p)
+            !~lev[e[i].v] && e[i].cc > 0 && (lev[q[++tl] = e[i].v] = lev[nw] + 1);
+    }
+
+    return ~lev[t];
+}
+
+inline ll dfs(int x, int t, ll lim)
+{
+    if (x == t)
+        return lim;
+
+    ll sf = 0;
+    for (int &i = cur[x]; ~i; i = e[i].p)
+    {
+        int to = e[i].v, pf;
+        if (lev[to] == lev[x] + 1 && e[i].cc > 0)
+        {
+            if ((pf = dfs(to, t, min(lim - sf, e[i].cc))))
+            {
+                e[i].cc -= pf;
+                e[i ^ 1].cc += pf;
+                sf += pf;
+            }
+        }
+    }
+    return sf;
+}
+
+inline ll dinic(int s, int t)
+{
+    ll fl = 0;
+    while (spfa(s, t))
+    {
+        ll pf;
+        while ((pf = dfs(s, t, INF)))
+            fl += pf;
+    }
+    return fl;
+}
+int get(int i, int j)
+{
+    return (i - 1) * m + j;
+}
+int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
+int main()
+{
+    scanf("%d%d", &n, &m);
+    int s = 0, t = 8 * n * m, bid = n * m;
+    for (int i = s; i <= t; ++i)
+        ed[i] = -1;
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= m; ++j)
+        {
+            int a;
+            scanf("%d", &a);
+            res += a;
+            add(s, get(i, j), a);
+        }
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= m; ++j)
+        {
+            int b;
+            scanf("%d", &b);
+            res += b;
+            add(get(i, j), t, b);
+        }
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= m; ++j)
+        {
+            int x;
+            scanf("%d", &x);
+            res += x;
+            add(s, ++bid, x);
+            add(bid, get(i, j), INF);
+            for (int k = 0; k < 4; ++k)
+            {
+                int ni = i + dx[k], nj = j + dy[k];
+                if (ni >= 1 && ni <= n && nj >= 1 && nj <= m)
+                    add(bid, get(ni, nj), INF);
+            }
+        }
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= m; ++j)
+        {
+            int x;
+            scanf("%d", &x);
+            res += x;
+            add(++bid, t, x);
+            add(get(i, j), bid, INF);
+            for (int k = 0; k < 4; ++k)
+            {
+                int ni = i + dx[k], nj = j + dy[k];
+                if (ni >= 1 && ni <= n && nj >= 1 && nj <= m)
+                    add(get(ni, nj), bid, INF);
+            }
+        }
+    printf("%lld\n", res - dinic(s, t));
+    return 0;
+}
